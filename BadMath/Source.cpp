db90f8ff1522e808084122b36be4b0ed1ed39db9
Jayden Luedeke <jayden.luedeke@sinclair.edu>
2023-04-14 21:19:27 -0400

    Fixed changes, see comments in code

--- BadMath/Source.cpp
+++ BadMath/Source.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 float average(int i1, int i2, int i3)
 {
-	return i1 + i2 + i3 / 3.0f;
+	return (i1 + i2 + i3) / 3.0f; //added parenthesis to follow proper order of operations
 }
 
 int main()
@@ -12,8 +12,8 @@ int main()
 	int n1 =0 , n2 = 0, n3 = 0;
 	cout << "This program calculates the average of three numbers." << endl;
 	cout << "First number: "; cin >> n1;
-	cout << "Second number: "; cin >> n2;
-	cout << "Third number: "; cin >> n2;
+	cout << "Second number: "; cin >> n2; 
+	cout << "Third number: "; cin >> n3; //changed cin >> n2 to cin n >> 3
 
 	float a = average(n1, n2, n3);
 
